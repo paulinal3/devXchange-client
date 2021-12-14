195b68ce8342c7a865a1f01952859bcd7a14a3c3
galyverasi <galyverasi@gmail.com>
2021-12-13 19:54:50 -0700

    Home and Detail working

--- src/components/Problems/Problem.js
+++ src/components/Problems/Problem.js
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom'
 import Details from './Details'
+import moment from 'moment'
 
 function Problem(props) {
     <Details />
     return (
-        <Link to={`/problems/${props.problem._id}`}state={props.currentProblem}>{props.problem.title}</Link>
+        <>
+            <Link to={`/problems/${props.problem._id}`} state={props.currentProblem}>{props.problem.title}</Link>
+            <p>Asked: {moment(props.problem.createdAt).fromNow()}</p>
+            <p>by {props.problem._id}</p>
+        </>
     )
 }
 
