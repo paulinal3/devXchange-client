8cdf6f90d0154498f8471527fd6365c2cfd5d690
paulinal3 <paulinal3@outlook.com>
2021-12-15 09:47:00 -0500

    commiting merge and first initial of last name appearing on show problems page

--- src/components/Problems/ShowProblem.js
+++ src/components/Problems/ShowProblem.js
@@ -7,6 +7,7 @@ function ShowProblem(props) {
     console.log('this is the problem id:', problemId)
     let currentProblem = props.problems && props.problems.find(x => x._id == problemId)
     console.log('this is the current problem\n', currentProblem)
+    let lastNameInit = currentProblem.owner.lastName.charAt(0)
     
      function deleteProblem(problemId)
     {
@@ -23,7 +24,7 @@ function ShowProblem(props) {
     return (
         <>
             <h3>{currentProblem.title}</h3>
-            <small>Asked by: {currentProblem.owner.firstName}</small>
+            <small>Asked by: {currentProblem.owner.firstName} {lastNameInit}.</small>
             <hr />
             <p>{currentProblem.description}</p>
             <p>{currentProblem.answers}</p>
