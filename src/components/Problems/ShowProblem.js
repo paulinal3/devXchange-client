8a2cf321cd620aed9d5d20530f3c300c709a4c05
galyverasi <galyverasi@gmail.com>
2021-12-15 14:58:38 -0700

    adding 'new' problems

--- src/components/Problems/ShowProblem.js
+++ src/components/Problems/ShowProblem.js
@@ -1,8 +1,11 @@
 import { useLocation, useNavigate, Link } from 'react-router-dom'
 import { destroyProblem } from '../../api/problems'
+import apiUrl from '../../apiConfig'
 
 function ShowProblem(props) {
-    const { pathname} = useLocation()
+    // const [newSolution, setNewSolution] = useState('')
+
+    const { pathname } = useLocation()
     const problemId = pathname.split('/')[2]
     console.log('this is the problem id:', problemId)
     let currentProblem = props.problems && props.problems.find(x => x._id == problemId)
@@ -12,7 +15,7 @@ function ShowProblem(props) {
 
     const deleteProblem = () => {
         destroyProblem(props.user, currentProblem._id)
-        // console.log('THIS IS:', `${apiUrl}/problems/${itemId}`)
+            // console.log('THIS IS:', `${apiUrl}/problems/${itemId}`)
             .then(() => {
                 props.refreshProblems()
                 navigate('/problems')
@@ -22,26 +25,51 @@ function ShowProblem(props) {
             })
     }
 
-return (
-    <>
-        <h3>{currentProblem.title}</h3>
-        <small>Asked by: {currentProblem.owner.firstName} {lastNameInit}.</small>
-        <hr />
-        <p>{currentProblem.description}</p>
-        <p>{currentProblem.answers}</p>
-        <button onClick={() => deleteProblem(props.user, currentProblem._id)}>Delete</button>
-        <Link to={`/problems/edit/${currentProblem._id}`}><button>Edit</button></Link>
+    // const createAnswer = () => {
+    //     postAnswer(props.user, currentProblem._id)
+    //         .then(() => {
+    //             setNewSolution('')
+    //         })
+    //         .catch(err => {
+    //             console.error(err)
+    //         })
+    // }
 
-        <p>Your Answer</p>
-        <form >
-            <label>
-                <textarea rows="5" cols="50" autofocus />
-            </label>
-            <br />
-            <input type="submit" value="Post Your Answer" />
-        </form>
-    </>
-)
+    // const postAnswer = (user, currentProblem._id) => {
+    //     return axios({
+    //         method: 'POST',
+    //         url: `${apiUrl}/answers`,
+    //         headers: {
+    //             Authorization: `Token token=${user.token}`
+    //         },
+    //         data: {
+    //             answer: {
+    //                 solution: newSolution.title,
+    //             }
+    //         }
+    //     })
+    // }
+
+    return (
+        <>
+            <h3>{currentProblem.title}</h3>
+            <small>Asked by: {currentProblem.owner.firstName} {lastNameInit}.</small>
+            <hr />
+            <p>{currentProblem.description}</p>
+            <p>{currentProblem.answers}</p>
+            <button onClick={() => deleteProblem(props.user, currentProblem._id)}>Delete</button>
+            <Link to={`/problems/edit/${currentProblem._id}`}><button>Edit</button></Link>
+
+            <p>Your Answer</p>
+            <form >
+                <label>
+                    <textarea rows="5" cols="50" autofocus />
+                </label>
+                <br />
+                {/* <input type="button" value="Post Your Answer" onclick={() => createAnswer()} /> */}
+            </form>
+        </>
+    )
 }
 
 export default ShowProblem
