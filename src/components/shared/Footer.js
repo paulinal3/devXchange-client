58f7362afb1898a09fa48757344d80135ae28a2b
paulinal3 <paulinal3@outlook.com>
2021-12-17 09:34:54 -0500

    footer links will open in a new tab now

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,69 +1,69 @@
 export default function Footer() {
     return (
-        <footer class="bg-dark text-center text-white">
+        <footer class='bg-dark text-center text-white'>
             {/* <!-- Grid container --> */}
-            <div class="container p-4">
+            <div class='container p-4'>
                 {/* <!-- Section: Social media --> */}
-                <section class="mb-4">
+                <section class='mb-4'>
                     {/* <!-- Facebook --> */}
-                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                    ><i class="fab fa-facebook-f"></i
+                    <a class='btn btn-outline-light btn-floating m-1' href='#!' role='button'
+                    ><i class='fab fa-facebook-f'></i
                     ></a>
 
                     {/* <!-- Twitter --> */}
-                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                    ><i class="fab fa-twitter"></i
+                    <a class='btn btn-outline-light btn-floating m-1' href='#!' role='button'
+                    ><i class='fab fa-twitter'></i
                     ></a>
 
                     {/* <!-- Google --> */}
-                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                    ><i class="fab fa-google"></i
+                    <a class='btn btn-outline-light btn-floating m-1' href='#!' role='button'
+                    ><i class='fab fa-google'></i
                     ></a>
 
                     {/* <!-- Instagram --> */}
-                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                    ><i class="fab fa-instagram"></i
+                    <a class='btn btn-outline-light btn-floating m-1' href='#!' role='button'
+                    ><i class='fab fa-instagram'></i
                     ></a>
 
                     {/* <!-- Linkedin --> */}
-                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                    ><i class="fab fa-linkedin-in"></i
+                    <a class='btn btn-outline-light btn-floating m-1' href='#!' role='button'
+                    ><i class='fab fa-linkedin-in'></i
                     ></a>
 
                     {/* <!-- Github --> */}
-                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                    ><i class="fab fa-github"></i
+                    <a class='btn btn-outline-light btn-floating m-1' href='#!' role='button'
+                    ><i class='fab fa-github'></i
                     ></a>
                 </section>
                 {/* <!-- Section: Social media --> */}
 
                 {/* <!-- Section: Form --> */}
-                {/* <section class="">
-                        <form action="">
+                {/* <section class=''>
+                        <form action=''>
                             <!--Grid row-->
-                            <div class="row d-flex justify-content-center">
+                            <div class='row d-flex justify-content-center'>
                                 <!--Grid column-->
-                                <div class="col-auto">
-                                    <p class="pt-2">
+                                <div class='col-auto'>
+                                    <p class='pt-2'>
                                         <strong>Sign up for our newsletter</strong>
                                     </p>
                                 </div>
                                 <!--Grid column-->
 
                                 <!--Grid column-->
-                                <div class="col-md-5 col-12">
+                                <div class='col-md-5 col-12'>
                                     <!-- Email input -->
-                                    <div class="form-outline form-white mb-4">
-                                        <input type="email" id="form5Example21" class="form-control" />
-                                        <label class="form-label" for="form5Example21">Email address</label>
+                                    <div class='form-outline form-white mb-4'>
+                                        <input type='email' id='form5Example21' class='form-control' />
+                                        <label class='form-label' for='form5Example21'>Email address</label>
                                     </div>
                                 </div>
                                 <!--Grid column-->
 
                                 <!--Grid column-->
-                                <div class="col-auto">
+                                <div class='col-auto'>
                                     <!-- Submit button -->
-                                    <button type="submit" class="btn btn-outline-light mb-4">
+                                    <button type='submit' class='btn btn-outline-light mb-4'>
                                         Subscribe
                                     </button>
                                 </div>
@@ -75,7 +75,7 @@ export default function Footer() {
                 {/* <!-- Section: Form --> */}
 
                 {/* <!-- Section: Text --> */}
-                <section class="mb-4">
+                <section class='mb-4'>
                     <p>
                         Three software engineers came together to develop a full-stack CRUD application that provides users the ability to post coding probelms they may be having and/or offer their coding knowlodege to help those who've hit a wall.
                     </p>
@@ -83,14 +83,14 @@ export default function Footer() {
                 {/* <!-- Section: Text --> */}
 
                 {/* <!-- Section: Links --> */}
-                <section class="">
+                <section class=''>
                     {/* <!--Grid row--> */}
-                    <div class="row">
+                    <div class='row'>
                         {/* <!--Grid column--> */}
-                        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                            <h5 class="text-uppercase">Tech Stack</h5>
+                        <div class='col-lg-3 col-md-6 mb-4 mb-md-0'>
+                            <h5 class='text-uppercase'>Tech Stack</h5>
 
-                            <ul class="list-unstyled mb-0">
+                            <ul class='list-unstyled mb-0'>
                                 <li>
                                     React
                                 </li>
@@ -108,63 +108,63 @@ export default function Footer() {
                         {/* <!--Grid column--> */}
 
                         {/* <!--Grid column--> */}
-                        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                            <h5 class="text-uppercase">Christos M.</h5>
+                        <div class='col-lg-3 col-md-6 mb-4 mb-md-0'>
+                            <h5 class='text-uppercase'>Christos M.</h5>
 
-                            <ul class="list-unstyled mb-0">
+                            <ul class='list-unstyled mb-0'>
                                 <li>
                                     cmiltiad@gmail.com
                                 </li>
                                 <li>
-                                    <a href="https://www.linkedin.com/in/christos-miltiadou/" class="text-white">Linkedin</a>
+                                    <a href='https://www.linkedin.com/in/christos-miltiadou/' class='text-white' target='_blank' rel='noopener noreferrer'>Linkedin</a>
                                 </li>
                                 <li>
-                                    <a href="https://github.com/cmiltiadou" class="text-white">GitHub</a>
+                                    <a href='https://github.com/cmiltiadou' class='text-white' target='_blank' rel='noopener noreferrer'>GitHub</a>
                                 </li>
                                 <li>
-                                    <a href="#!" class="text-white">Portfolio</a>
+                                    <a href='#!' class='text-white' target='_blank' rel='noopener noreferrer'>Portfolio</a>
                                 </li>
                             </ul>
                         </div>
                         {/* <!--Grid column--> */}
 
                         {/* <!--Grid column--> */}
-                        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                            <h5 class="text-uppercase">Galyver A.</h5>
+                        <div class='col-lg-3 col-md-6 mb-4 mb-md-0'>
+                            <h5 class='text-uppercase'>Galyver A.</h5>
 
-                            <ul class="list-unstyled mb-0">
+                            <ul class='list-unstyled mb-0'>
                                 <li>
                                     galyverasi@gmail.com
                                 </li>
                                 <li>
-                                    <a href="https://www.linkedin.com/in/galyverasi/" class="text-white">Linkedin</a>
+                                    <a href='https://www.linkedin.com/in/galyverasi/' class='text-white' target='_blank' rel='noopener noreferrer'>Linkedin</a>
                                 </li>
                                 <li>
-                                    <a href="https://github.com/galyverasi" class="text-white">GitHub</a>
+                                    <a href='https://github.com/galyverasi' class='text-white' target='_blank' rel='noopener noreferrer'>GitHub</a>
                                 </li>
                                 <li>
-                                    <a href="#!" class="text-white">Portfolio</a>
+                                    <a href='#!' class='text-white' target='_blank' rel='noopener noreferrer'>Portfolio</a>
                                 </li>
                             </ul>
                         </div>
                         {/* <!--Grid column--> */}
 
                         {/* <!--Grid column--> */}
-                        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                            <h5 class="text-uppercase">Paulina L.</h5>
+                        <div class='col-lg-3 col-md-6 mb-4 mb-md-0'>
+                            <h5 class='text-uppercase'>Paulina L.</h5>
 
-                            <ul class="list-unstyled mb-0">
+                            <ul class='list-unstyled mb-0'>
                                 <li>
                                     paulinal3@outlook.com
                                 </li>
                                 <li>
-                                    <a href="https://www.linkedin.com/in/paulinal3/" class="text-white">Linkedin</a>
+                                    <a href='https://www.linkedin.com/in/paulinal3/' class='text-white' target='_blank' rel='noopener noreferrer'>Linkedin</a>
                                 </li>
                                 <li>
-                                    <a href="https://github.com/paulinal3" class="text-white">GitHub</a>
+                                    <a href='https://github.com/paulinal3' class='text-white' target='_blank' rel='noopener noreferrer'>GitHub</a>
                                 </li>
                                 <li>
-                                    <a href="#!" class="text-white">Portfolio</a>
+                                    <a href='#!' class='text-white' target='_blank' rel='noopener noreferrer'>Portfolio</a>
                                 </li>
                             </ul>
                         </div>
@@ -176,9 +176,9 @@ export default function Footer() {
             </div>
             {/* <!-- Grid container --> */}
 
-            <div class="text-center p-3" styles="background-color: rgba(0, 0, 0, 0.2);">
+            <div class='text-center p-3' styles='background-color: rgba(0, 0, 0, 0.2);'>
                 Built By: Solo Tres
-                {/* <a class="text-white" href="https://mdbootstrap.com/"> Solo Tres</a> */}
+                {/* <a class='text-white' href='https://mdbootstrap.com/'> Solo Tres</a> */}
             </div>
         </footer>
     )
