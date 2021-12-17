8e2dc159605cc7b2892c5e98b809e64d213e35e5
galyverasi <galyverasi@gmail.com>
2021-12-17 14:43:42 -0700

    added hero on all landing pages

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -14,65 +14,75 @@ const SignIn = (props) => {
 
     const navigate = useNavigate()
 
-	const onSignIn = (event) => {
-		event.preventDefault()
+    const onSignIn = (event) => {
+        event.preventDefault()
         console.log('the props', props)
-		const { msgAlert, setUser } = props
+        const { msgAlert, setUser } = props
 
-        const credentials = {email, password}
+        const credentials = { email, password }
 
-		signIn(credentials)
-			.then((res) => setUser(res.data.user))
-			.then(() =>
-				msgAlert({
-					heading: 'Sign In Success',
-					message: messages.signInSuccess,
-					variant: 'success',
-				})
-			)
-			.then(() => navigate('/'))
-			.catch((error) => {
+        signIn(credentials)
+            .then((res) => setUser(res.data.user))
+            .then(() =>
+                msgAlert({
+                    heading: 'Sign In Success',
+                    message: messages.signInSuccess,
+                    variant: 'success',
+                })
+            )
+            .then(() => navigate('/'))
+            .catch((error) => {
                 setEmail('')
                 setPassword('')
-				msgAlert({
-					heading: 'Sign In Failed with error: ' + error.message,
-					message: messages.signInFailure,
-					variant: 'danger',
-				})
-			})
-	}
+                msgAlert({
+                    heading: 'Sign In Failed with error: ' + error.message,
+                    message: messages.signInFailure,
+                    variant: 'danger',
+                })
+            })
+    }
 
     return (
-        <div className='row'>
-            <div className='col-sm-10 col-md-8 mx-auto mt-5'>
-                <h3>Sign In</h3>
-                <Form onSubmit={onSignIn}>
-                    <Form.Group controlId='email'>
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control
-                            required
-                            type='email'
-                            name='email'
-                            value={email}
-                            placeholder='Enter email'
-                            onChange={e => setEmail(e.target.value)}
-                        />
-                    </Form.Group>
-                    <Form.Group controlId='password'>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                            required
-                            name='password'
-                            value={password}
-                            type='password'
-                            placeholder='Password'
-                            onChange={e => setPassword(e.target.value)}
-                        />
-                    </Form.Group>
-                    <Button variant='primary' type='submit'>
-                        Submit
-                    </Button>
-                </Form>
+        <div>
+            <div>
+                {/* Jumbotron */}
+                <div class="container-fluid bg-dark text-light p-5">
+                    <h1 class="mb-3">Welcome back</h1>
+                    <h4 class="mb-3">Collaborate and share knowledge for free</h4>
+                </div>
+                {/* Jumbotron */}
+            </div>
+            <div className='row'>
+                <div className='col-sm-10 col-md-8 mx-auto mt-5'>
+                    <h3>Sign In</h3>
+                    <Form onSubmit={onSignIn}>
+                        <Form.Group controlId='email'>
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                required
+                                type='email'
+                                name='email'
+                                value={email}
+                                placeholder='Enter email'
+                                onChange={e => setEmail(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId='password'>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                required
+                                name='password'
+                                value={password}
+                                type='password'
+                                placeholder='Password'
+                                onChange={e => setPassword(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Button variant='primary' type='submit'>
+                            Submit
+                        </Button>
+                    </Form>
+                </div>
             </div>
         </div>
     )
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -53,10 +53,9 @@ const SignUp = (props) => {
         <div>
             <div>
                 {/* Jumbotron */}
-                <div class="p-5 text-center bg-light">
-                    <h1 class="mb-3">Hello</h1>
-                    <h4 class="mb-3">Subheading</h4>
-                    <a class="btn btn-primary" href="" role="button">Call to action</a>
+                <div class="container-fluid bg-dark text-light p-5">
+                    <h1 class="mb-3">Join the DevXChange community </h1>
+                    <h4 class="mb-3">Collaborate and share knowledge for free</h4>
                 </div>
                 {/* Jumbotron */}
             </div>
