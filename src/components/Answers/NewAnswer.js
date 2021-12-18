53dc77f4fe846f9315c8f4f2c9ecddce6aeaffbe
galyverasi <galyverasi@gmail.com>
2021-12-18 13:00:33 -0700

    conditonal on user not able to answer their problem

--- src/components/Answers/NewAnswer.js
+++ src/components/Answers/NewAnswer.js
@@ -5,15 +5,16 @@ export default function NewAnswer(props) {
         <div>
             <Accordion id='newAnswer' defaultActiveKey="0">
                 <Accordion.Item eventKey="0">
-                    <>
-                        <Accordion.Header>Post Your Answer</Accordion.Header>
-                        <Accordion.Body>
-                            <Form>
-                                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                                    <Form.Control as="textarea" rows={7} type="text" name='solution' value={props.newSolution.solution} onChange={props.handleAnswer} />
-                                </Form.Group>
-                                <Button onClick={() => props.createAnswer()}>Post Answer</Button>
-                                {/* <input
+                    {props.user && props.user._id != props.currentProblem.owner._id &&
+                        <>
+                            <Accordion.Header>Post Your Answer</Accordion.Header>
+                            <Accordion.Body>
+                                <Form>
+                                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                        <Form.Control as="textarea" rows={7} type="text" name="solution" value={props.newSolution.solution} onChange={props.handleAnswer} />
+                                    </Form.Group>
+                                    <Button onClick={() => props.createAnswer()}>Post Answer</Button>
+                                    {/* <input
                                 id='solution'
                                 type='text'
                                 name='solution'
@@ -25,9 +26,10 @@ export default function NewAnswer(props) {
                                 value='Post Your Answer'
                                 onClick={() => props.createAnswer()}
                             /> */}
-                            </Form>
-                        </Accordion.Body>
-                    </>
+                                </Form>
+                            </Accordion.Body>
+                        </>
+                    }
                 </Accordion.Item>
             </Accordion>
         </div>
