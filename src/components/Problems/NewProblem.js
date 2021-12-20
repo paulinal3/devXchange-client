c005ee5d30bc1c41632c393500c53f90ee4c8bcd
paulinal3 <paulinal3@outlook.com>
2021-12-19 22:35:40 -0500

    fixing errors from last merge

--- src/components/Problems/NewProblem.js
+++ src/components/Problems/NewProblem.js
@@ -17,8 +17,6 @@ export default function NewProblem(props) {
         img: ''
     })
 
-    
-
     let modules = {
         syntax: true,
         toolbar: [
@@ -38,7 +36,6 @@ export default function NewProblem(props) {
 
     // helper method attached to button
     const createNewProblem = () => {
-        
         // axios call to create the new problem in the db
         postProblem(props.user, newProblem, value)
             // console.log('this is the current user id:', user._id)
@@ -60,54 +57,52 @@ export default function NewProblem(props) {
 
     return (
         <div>
-            {/* <----- Jumbotron -----> */}
-            <div class='container-fluid bg-dark text-light p-5'>
-                <h1 class='mb-3'>Tips on posting a problem</h1>
-                <h4 class='mb-3'>
-                    <ol>
-                        <li>Summarize the problem</li>
-                        <li>Describe what you've tried</li>
-                        <li>When appropriate, show some code</li>
-                    </ol>
-                </h4>
-            </div>
-            {/* <----- Jumbotron -----> */}
-
-            {/* <----- Form to Create a New Problem -----> */}
-            <Form id='newProbForm'>
-
-                <Form.Group className='mb-3' controlId='title' style={{'margin-top':'20px'}}>
-                    <FloatingLabel
-                        controlId='floatingInput'
-                        label='Title'
-                        className='mb-3'
-                    >
-                    <Form.Control 
-                        type='text' 
-                        name='title' 
-                        value={newProblem.title} 
-                        onChange={handleChange}
-                        placeholder='Title' 
-                    />
-                    </FloatingLabel>
-
-                </Form.Group>
-
-                <Form.Group className='mb-3'>
-                    <ReactQuill 
-                    style={{'height':'100%'}}
-                    id = "textEditor"
-                    theme="snow" 
-                    name='description' 
-                    modules = {modules}
-                    value={value} 
-                    onChange={setValue}
-                    placeholder='describe your problem...'
-                    />
-                </Form.Group>
-                <Button id='formBtn' onClick={() => createNewProblem()}>Post Problem</Button>
-            </Form>
-            {/* <----- Form to Create a New Problem -----> */}
+        {/* <----- Jumbotron -----> */}
+        <div class='container-fluid bg-dark text-light p-5'>
+            <h1 class='mb-3'>Tips on posting a problem</h1>
+            <h4 class='mb-3'>
+                <div id='problemTips'>
+                    <p>Summarize the problem</p>
+                    <p>•</p> 
+                    <p>Describe what you've tried</p>
+                    <p>•</p>
+                    <p>When appropriate, show some code</p>
+                </div>
+            </h4>
         </div>
+
+        {/* <----- Form to Create a New Problem -----> */}
+        <Form id='newProbForm'>
+            <Form.Group className='mb-3' controlId='title' style={{'margin-top':'20px'}}>
+                <FloatingLabel
+                    controlId='floatingInput'
+                    label='Title'
+                    className='mb-3'
+                >
+                <Form.Control 
+                    type='text' 
+                    name='title' 
+                    value={newProblem.title} 
+                    onChange={handleChange}
+                    placeholder='Title' 
+                />
+                </FloatingLabel>
+
+            </Form.Group>
+            <Form.Group className='mb-3'>
+                <ReactQuill 
+                style={{'height':'100%'}}
+                id = "textEditor"
+                theme="snow" 
+                name='description' 
+                modules = {modules}
+                value={value} 
+                onChange={setValue}
+                placeholder='describe your problem...'
+                />
+            </Form.Group>
+            <Button id='formBtn' onClick={() => createNewProblem()}>Post Problem</Button>
+        </Form>
+    </div>
     )
 }
