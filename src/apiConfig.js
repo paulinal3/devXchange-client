5e5b016b0176ea2d4679ec2dc33f3af1cb2d1399
paulinal3 <paulinal3@outlook.com>
2021-12-19 22:13:09 -0500

    hopefully fixing heroku login issue

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: 'https://devxchange-server.herokuapp.com/',
+	production: 'https://devxchange-server.herokuapp.com',
 	development: 'http://localhost:8000',
 }
 
